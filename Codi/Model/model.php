ce8f4cdca8359d4a8a22667b57200ed00dad4ddc
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2024-02-16 15:50:32 +0100

    model corregit

--- Codi/Model/model.php
+++ Codi/Model/model.php
@@ -22,29 +22,25 @@
     }
     function isProfessor($email){
         $connexio = connect();
-        $sql = "SELECT correu FROM professor";
+        $sql = "SELECT correu FROM professor WHERE correu = '$email'";
         $result = $connexio->query($sql);
         $result = $result->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $row) {
-            if($row['correu'] == $email){
-                return true;
-            }else{
-                return false;
-            }
+        if($result){
+            return true;
+        }else{
+            return false;
         }
         $connexio = null;
     }
     function isAdmin($email){
         $connexio = connect();
-        $sql = "SELECT correu FROM admin";
+        $sql = "SELECT correu FROM admin WHERE correu = '$email'";
         $result = $connexio->query($sql);
         $result = $result->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $row) {
-            if($row['correu'] == $email){
-                return true;
-            }else{
-                return false;
-            }
+        if($result){
+            return true;
+        }else{
+            return false;
         }
         $connexio = null;
     }
