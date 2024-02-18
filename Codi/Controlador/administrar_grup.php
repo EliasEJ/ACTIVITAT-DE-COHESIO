4be4f52d9192b176a70f85b97f12bd72c7804a07
martinh118 <m.jaime@sapalomera.cat>
2024-02-18 18:17:12 +0100

    cambios Import, administracion grupo

--- Codi/Controlador/administrar_grup.php
+++ Codi/Controlador/administrar_grup.php
@@ -3,39 +3,57 @@ require_once("../Vista/index_professor.php");
 require_once("../Model/model.php");
 require_once("../Model/model_professor.php");
 
-if(isset($_GET['accio'])){
+if (isset($_GET['accio'])) {
     $accion = $_GET['accio'];
 
-    switch($accion){
+    switch ($accion) {
         case "eliminar":
             $grupoId = $_GET['idGrup'];
-            //comprobarGrupos($grupoId);
-        break;
+
+
+            if (comprobarGrupos($grupoId,  $_SESSION['idProfe'])) {
+                eliminarGrup($grupoId);
+            } else {
+?>
+                <script>
+                    alert("No és possible esborrar aquest grup: Encara hi ha alumnes que pertanyen a aquest grup.")
+                </script>
+<?php
+            }
+            break;
         case "crear":
             $grupos = obtenirGrups()->fetchAll();
             $id = count($grupos) + 1;
-            $nombre = "Grup-".$id;
+            $nombre = "Grup-" . $id;
             $imagen = "";
             $puntuacion = 0;
             $profeId = 1; //Cambiar id al de sesión.
-            
+
             crearGrup($id, $nombre, $imagen, $puntuacion, $profeId);
             break;
         default:
 
-        break;
+            break;
     }
-}else if($_SERVER["REQUEST_METHOD"] == "POST"){
+} else if ($_SERVER["REQUEST_METHOD"] == "POST") {
     foreach ($_POST['alumne_id'] as $alumne_id) {
         // Obtener el nuevo identificador de grupo seleccionado para este alumno
         $nuevo_grup_id = $_POST['grup'][$alumne_id];
-        
+
         modificarGrupUsuari($alumne_id, $nuevo_grup_id);
     }
 }
 
-function comprobarGrupos($grupId, $idTutor){
-    $grupos = obtenirAlumnat($idTutor);
+function comprobarGrupos($grupId, $idTutor)
+{
+    $alumnos = obtenirAlumnat($idTutor)->fetchAll();
+
+    foreach ($alumnos as $al) {
+        if ($al['grup_id'] == $grupId) {
+            return false;
+        }
+    }
+    return true;
 }
 
 ?>
