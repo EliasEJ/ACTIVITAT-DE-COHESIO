b74273a0c2a0c9ad36217b479c11de4edf03f500
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2024-03-11 15:43:30 +0100

    ruta config

--- Recursos/configuracio.php
+++ Recursos/configuracio.php
@@ -3,9 +3,9 @@
 
   $clientID = '284388371606-6du1403daqmins6ic96hpggtuqt3pbl3.apps.googleusercontent.com';
   $clientSecret = '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';
-  // $redirectUri = 'http://localhost/cohesio/ACTIVITAT-DE-COHESIO/Codi/Controlador/controlador.php';
+   $redirectUri = 'http://localhost/cohesio/ACTIVITAT-DE-COHESIO/Codi/Controlador/controlador.php';
   //$redirectUri = 'http://localhost/ACTIVITAT-DE-COHESIO/Codi/Controlador/controlador.php';
-  $redirectUri = 'http://localhost/practiques/ACTIVITAT-DE-COHESIO/Codi/Controlador/controlador.php';
+  //$redirectUri = 'http://localhost/practiques/ACTIVITAT-DE-COHESIO/Codi/Controlador/controlador.php';
 
 
   // create Client Request to access Google API
