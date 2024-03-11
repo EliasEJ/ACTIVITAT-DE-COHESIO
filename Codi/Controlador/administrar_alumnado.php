64c029f85a0bc2ad778943a64e81e844a9fc226d
martinh118 <m.jaime@sapalomera.cat>
2024-03-11 17:01:26 +0100

    avance en guardar confirmacion asistencia.

--- Codi/Controlador/administrar_alumnado.php
+++ Codi/Controlador/administrar_alumnado.php
@@ -5,37 +5,41 @@ require_once("../Model/model.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if(isset($_POST['guardarAlumno'])){
+    if (isset($_POST['guardarAlumno'])) {
         $profeId =  $_SESSION['idProfe'];
         $alumnadoProfe = obtenirAlumnat($profeId)->fetchAll();
-    
+
         $curso = $alumnadoProfe[0]['curs'];
         $año = $alumnadoProfe[0]['any'];
         $clase = $alumnadoProfe[0]['classe'];
-    
+
         $alumnos = obtenerAlumnosTotal()->fetchAll();
         $newId = count($alumnos) + 1;
         $alumnName = $_POST['newAlumnNombre'];
         $alumnCognom = $_POST['newAlumnApellidos'];
         $alumnCorreu = $_POST['newAlumnCorreu'];
         $grupoId = $_POST['grup'];
-    
+
         crearAlumno($newId, $alumnName, $alumnCognom, $alumnCorreu, $curso, $año, $clase, 1, 1, $grupoId, $profeId);
-        
-        ?>
+
+?>
         <script>
             alert("Alumne afegit correctament.")
         </script>
-        <?php
+    <?php
 
-    }else if(isset($_POST['guardarAsistencia'])){
-        
-        echo $_POST['asistAlumn'];
-
+    } else if (isset($_POST['datos'])) {
+       
+        $listaAsistencia = $_POST['datos'];
+        foreach ($listaAsistencia as $alumn) {
+            echo $alumn['id'] . ": " . $alumn['asistencia'];
+        }
     }
-   
+    
+
+
 }
 ?>
 <script>
-    location.replace("../Vista/index_professor.php")
+    //location.replace("../Vista/index_professor.php")
 </script>
